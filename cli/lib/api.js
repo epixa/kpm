3d6186076e94601f48f105e219325c3de5725815
Court Ewing <court@epixa.com>
2016-02-07 09:01:07 -0500

    Todo for prepending api base url

--- cli/lib/api.js
+++ cli/lib/api.js
@@ -5,6 +5,7 @@ const curry = require('lodash').curry;
 const cbPromiseHandler = require('./util').cbPromiseHandler;
 
 function put() {
+  // todo: prepend base url
   // todo: inject authorization
   // todo: handle common errors
   return request.put.apply(request, arguments);
